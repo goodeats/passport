d8e4ddde19976a31c04cbeb3b9dbf50d230922d9
Pat Needham <patrick.h.needham@gmail.com>
2022-04-13 14:48:42 -0400

    react router dom working to change components when clicking on links

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Navbar from './Components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import AdminPage from './Pages/AdminPage';
+import LoginPage from './Pages/LoginPage';
+import ProfilePage from './Pages/ProfilePage';
+import './main.css'
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<HomePage/>}></Route>
+        <Route path='/admin' element={<AdminPage/>}></Route>
+        <Route path='/login' element={<LoginPage/>}></Route>
+        <Route path='/profile' element={<ProfilePage/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/Components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <div className='NavContainer'>
+      <Link to="/logout">Logout</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/">Home</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/profile">Profile</Link>
+      <Link to="/admin">Admin</Link>
+    </div>
+  )
+}
