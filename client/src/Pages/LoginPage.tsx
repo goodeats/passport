b8ee75a8354d1c27b9da34d3c6b3d92257938614
Pat Needham <patrick.h.needham@gmail.com>
2022-04-13 15:11:51 -0400

    login working from ui

--- client/src/Pages/LoginPage.tsx
+++ client/src/Pages/LoginPage.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 export default function LoginPage() {
+  const [username, setUsername] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+
+  const login = () => {
+    axios.post('http://localhost:4000/login', {
+      username,
+      password
+    }, {
+      withCredentials: true
+    }).then(res => {
+      console.log(res.data)
+    })
+  }
+
+  const getUser = () => {
+    axios.get('http://localhost:4000/user', {
+      withCredentials: true
+    }).then(res => {
+      console.log(res.data)
+    })
+  }
+
   return (
-    <div>LoginPage</div>
+    <div>
+      <h1>Login</h1>
+      <input type="text" name="username" onChange={e => setUsername(e.target.value)} />
+      <input type="text" name="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={login}>Login</button>
+      <button onClick={getUser}>Get Logged In User</button>
+    </div>
   )
 }
