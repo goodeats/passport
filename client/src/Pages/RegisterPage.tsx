7b913ec014a79dd8beb8a68c6d17810372e85226
Pat Needham <patrick.h.needham@gmail.com>
2022-04-14 14:27:55 -0400

    can register from front end now

--- client/src/Pages/RegisterPage.tsx
+++ client/src/Pages/RegisterPage.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react';
 
 export default function RegisterPage() {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const register = () => {
+    axios
+      .post(
+        'http://localhost:4000/register',
+        {
+          username,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        if (res.status === 200) {
+          window.location.href = '/login';
+        }
+      });
+  };
+
   return (
-    <div>RegisterPage</div>
-  )
+    <div>
+      <h1>Register</h1>
+      <input
+        type="text"
+        name="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        name="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={register}>Register</button>
+    </div>
+  );
 }
