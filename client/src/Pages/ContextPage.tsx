4533956c2cea5fa6682dcb7c3b40d3024f3c9588
Pat Needham <patrick.h.needham@gmail.com>
2022-04-14 13:46:25 -0400

    fixed interesting bug with useEffect memory leak -- now only firing once

--- client/src/Pages/ContextPage.tsx
+++ client/src/Pages/ContextPage.tsx
@@ -1,19 +1,32 @@
-import axios from 'axios'
-import React, { createContext, PropsWithChildren, useEffect, useState } from 'react'
+import axios from 'axios';
+import React, {
+  createContext,
+  PropsWithChildren,
+  useEffect,
+  useState,
+} from 'react';
 
 // https://reactjs.org/docs/context.html
-export const myContext = createContext<any>({})
+export const myContext = createContext<any>({});
 export default function ContextPage(props: PropsWithChildren<any>) {
   const [user, setUser] = useState<any>();
+
+  // specified an empty array as a dependency to the useEffect React hook
+  // So we have ensured that the fetch request happens only once
+  // www.wisdomgeek.com/development/web-development/react/avoiding-race-conditions-memory-leaks-react-useeffect/
+  // reactjs.org/docs/hooks-effect.html
   useEffect(() => {
-    axios.get('http://localhost:4000/user', {withCredentials: true}).then((res: { data: any; }) => {
-      setUser(res.data);
-    });
-  })
+    axios
+      .get('http://localhost:4000/user', { withCredentials: true })
+      .then((res: { data: any }) => {
+        setUser(res.data);
+        console.log('got user');
+      });
+  }, []);
 
   return (
     <myContext.Provider value={user}>
       {props.children}
     </myContext.Provider>
-  )
+  );
 }
