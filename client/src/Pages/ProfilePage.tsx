43a7f23c6175061c7fd66bcf32ad838d59c48f65
Pat Needham <patrick.h.needham@gmail.com>
2022-04-20 13:04:54 -0400

    context in profile page to display user's name

--- client/src/Pages/ProfilePage.tsx
+++ client/src/Pages/ProfilePage.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { myContext } from './ContextPage';
 
 export default function ProfilePage() {
+  const ctx = useContext(myContext);
+
   return (
-    <div>ProfilePage</div>
-  )
+    <div>
+      <h1>{ctx.username} Profile</h1>
+    </div>
+  );
 }
